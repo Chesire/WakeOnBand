2bbba89bc42a0f1e40084eb00a0090d1005344ac
Trey <troyrijkaard@hotmail.co.uk>
2016-04-07 21:41:22 +0100

    feat(receiver): add a receiver for button presses

---
+++ WakeOnBandXamarin/WakeOnBandXamarin/WakeOnBandXamarin.Droid/TileReceiver.cs
@@ -0,0 +1,43 @@
+using Android.Content;
+using Android.App;
+using Microsoft.Band.Tiles;
+
+namespace WakeOnBandXamarin.Droid
+{
+    [BroadcastReceiver(Enabled = true)]
+    [IntentFilter(new[] { TileEvent.ActionTileButtonPressed })]
+    public class TileReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            TileButtonEvent eventData = intent.Extras.Get(TileEvent.TileEventData) as TileButtonEvent;
+            if (eventData == null)
+                return;
+
+            // load which wol device to wake up
+
+            //throw new NotImplementedException();
+            /*
+            TileButtonEvent eventData = (TileButtonEvent)intent.getExtras().get("TILE_EVENT_DATA");
+            if (eventData != null && eventData.getPageID() != null)
+            {
+                boolean loadedModels;
+                WolDataService wolData = WolDataService.getInstance();
+                try
+                {
+                    wolData.loadWolModels(context);
+                    loadedModels = true;
+                }
+                catch (Exception e)
+                {
+                    loadedModels = false;
+                }
+
+                if (loadedModels)
+                {
+                    new WakeTask(eventData.getPageID()).execute();
+                }
+            }*/
+        }
+    }
+}
