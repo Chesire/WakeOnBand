9427c746fa706c198737502faf7ff982a7bd4d9c
Trey <troyrijkaard@hotmail.co.uk>
2016-04-11 22:39:59 +0100

    wip: add app class

---
+++ WakeOnBandXamarin/WakeOnBandXamarin/WakeOnBandXamarin/App.cs
@@ -0,0 +1,16 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using WakeOnBandXamarin.Interfaces;
+using WakeOnBandXamarin.Services;
+
+namespace WakeOnBandXamarin
+{
+    public class App : MvxApplication
+    {
+        public App()
+        {
+            Mvx.RegisterType<IBand, BandService>();
+            Mvx.RegisterType<IWol, WolService>();
+        }
+    }
+}
