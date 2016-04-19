d3ffd532e4272b01400ef7dfebe2cf08331322d7
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2016-04-19 21:33:45 +0100

    feat(wolmodel): add a model for woltargets

---
+++ WakeOnBandXamarin/WakeOnBandXamarin/WakeOnBandXamarin/Models/WolTargetModel.cs
@@ -0,0 +1,37 @@
+﻿namespace WakeOnBandXamarin.Core.Models
+{
+    /// <summary>
+    /// Provides a model for Wake on Lan targets
+    /// </summary>
+    public class WolTargetModel
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="name">Displayed name of the target</param>
+        /// <param name="macAddress">Mac address of the target</param>
+        public WolTargetModel(string name, string macAddress)
+        {
+            Name = name;
+            MacAddress = macAddress;
+        }
+
+        #endregion Constructor
+
+        #region Properties
+
+        /// <summary>
+        /// Displayed name of the target to wake
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Mac address of the target to wake
+        /// </summary>
+        public string MacAddress { get; private set; }
+
+        #endregion Properties
+    }
+}
